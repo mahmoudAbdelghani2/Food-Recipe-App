9ead205225784268a1ea97d5dccc02eaa6879b04
Mahmoud Ahmed Abdelghani <mahmoudabdelgani346@gmail.com>
2026-04-13 23:50:22 +0200

    implement UserModel and DatabaseHelper with local sqflite storage

---
+++ food_recipe_app/lib/Services/database_helper.dart
@@ -0,0 +1,67 @@
+import 'package:food_recipe_app/Models/user_model.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static Database? _database;
+  Future<Database?> get db async {
+    _database ??= await initDB();
+    return _database;
+  }
+
+  Future<Database> initDB() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'foodRecipe.db');
+    Database db = await openDatabase(
+      path,
+      onCreate: _onCreate,
+      version: 1,
+      onConfigure: _onConfigure,
+    );
+    return db;
+  }
+
+  Future _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+  CREATE TABLE Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    password TEXT,
+    gender TEXT
+  )
+''');
+
+    await db.execute('''
+  CREATE TABLE Favorites (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER,
+    recipeId INTEGER,
+    FOREIGN KEY (userId) REFERENCES Users (id) ON DELETE CASCADE
+  )
+''');
+  }
+
+  Future<int> insertUser(UserModel user) async {
+    final dbClient = await db;
+    return await dbClient!.insert('Users', user.toMap());
+  }
+
+  Future<UserModel?> checkUser(String email, String password) async {
+    final dbClient = await db;
+    var res = await dbClient!.query(
+      'Users',
+      where: 'email = ? AND password = ?',
+      whereArgs: [email, password],
+    );
+
+    if (res.isNotEmpty) {
+      return UserModel.fromMap(res.first);
+    }
+    return null; // لو البيانات غلط أو مش موجودة
+  }
+}
